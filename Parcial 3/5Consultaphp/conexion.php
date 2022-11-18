c72ed09fdcfc0f1129f9fab917171bc023426233
Roberto ópez <roberto.elbeto7@gmail.com>
2022-11-18 13:26:12 -0600

    act

--- Parcial 3/5Consultaphp/conexion.php
+++ Parcial 3/5Consultaphp/conexion.php
@@ -1,11 +1,11 @@
 <?php
+    
     $servidor = "localhost";
     $usuario = "root";
     $password = "1234567";
-    $basedatos = "Northwind"
+    $db = "Northwind";
 
-
-    $con = mysqli_conect($Servidor,$usuario,$password,$basedatos) or die("Problemas en la conexion a la base de datos");
+    $con = mysqli_conect($servidor,$usuario,$password,$db) or die("Problemas en la conexion a la base de datos");        
     $consulta = "select * from categories";
     $registros = mysqli_query($con,$consulta) or die("Problemas en el select");
 
