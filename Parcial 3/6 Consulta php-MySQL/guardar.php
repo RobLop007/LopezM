e8de286f99cd30b7795d2c4266c9d230522beedf
Roberto ópez <roberto.elbeto7@gmail.com>
2022-11-20 15:00:39 -0600

    act

--- Parcial 3/6 Consulta php-MySQL/guardar.php
+++ Parcial 3/6 Consulta php-MySQL/guardar.php
@@ -11,6 +11,16 @@
     $id = $_POST["#id"];
     $contraseña = $_POST["#contraseña"];
 
-    $insert = "INSERT INTO usuario values ('$nombres', '$apellido', '$correo', '$telefono', '$fecha', '$genero', )"
+    $insert = "INSERT INTO usuario values ('$nombres', '$apellido', '$correo', '$telefono', '$fecha', '$genero', '$id', '$contraseña')";
+
+    $query = mysqli_query($con, $insert);
+
+    if($query){
+
+        echo "correcto";
+    } else {
+        echo "incorrecto";
+    }
+
 
 ?>
