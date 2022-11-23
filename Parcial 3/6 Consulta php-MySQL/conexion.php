8e98144c041a3d0a3bc6bf7cbe6802baaf6d96a9
Roberto López <roberto.elbeto7@gmail.com>
2022-11-23 07:38:00 -0600

    act

--- Parcial 3/6 Consulta php-MySQL/conexion.php
+++ Parcial 3/6 Consulta php-MySQL/conexion.php
@@ -1,17 +1,17 @@
 <?php
     
     $servidor = "localhost";
+    $basedatos = "Northwind";
     $usuario = "root";
     $password = "";
-    $db = "proyect_web";
 
-    $con = mysqli_conect($servidor,$usuario,$password,$db);
-    
-    /*$consulta = "select * from categories";
+    $con = mysqli_conect($servidor,$usuario,$password,$basedatos) or die("Problemas al conectar con el servidor");
+
+    $consulta = "select * from categories";
     $registros = mysqli_query($con,$consulta) or die("Problemas en el select");
 
     $result = mysqli_fetch_all($registros);
     mysqli_close($con);
-    echo json_encode($result);*/
+    echo json_encode($result);
 
 ?>
