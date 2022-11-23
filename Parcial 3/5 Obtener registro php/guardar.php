f506ec5012b8824e3b0a9253239a06aef84b00d3
Roberto ópez <roberto.elbeto7@gmail.com>
2022-11-22 19:54:04 -0600

    act

---
+++ Parcial 3/5 Obtener registro php/guardar.php
@@ -0,0 +1,15 @@
+<?php
+
+    $registro["nombre"] = "Roberto";
+    $registro["apellido"] = "López";
+    $registro["correo"] = "rjlom001@gmail.com";
+    $registro["telefono"] = "8672445834";
+    $registro["fechaNac"] = "2000-10-18";
+    $registro["genero"] = "Masculino";
+    $registro["id"] = "RobLop007";
+    $registro["contraseña"] = "1234";
+    $registro["contraseñaVal"] = "1234";
+
+    echo json_encode($registro);
+
+?>
