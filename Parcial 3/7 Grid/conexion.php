c8e0e44f3a19b1003fce0ba1b9bcde0109313268
Roberto López <roberto.elbeto7@gmail.com>
2022-11-25 09:55:51 -0600

    Tarea Grid

---
+++ Parcial 3/7 Grid/conexion.php
@@ -0,0 +1,17 @@
+<?php
+    
+    $servidor = "localhost";
+    $basedatos = "proyect_web";
+    $usuario = "root";
+    $password = "1234567";
+
+    $con = mysqli_connect($servidor,$usuario,$password,$basedatos) or die("Problemas al conectar con el servidor");
+
+    $consulta = "select * from usuario";
+    $registros = mysqli_query($con,$consulta) or die("Problemas en el select");
+
+    $result = mysqli_fetch_all($registros,MYSQLI_ASSOC);
+    mysqli_close($con);
+    echo json_encode($result);
+
+?>
